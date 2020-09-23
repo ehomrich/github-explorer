7653057705a8e70ef66e210a7ace0386985301a4
Émerson P. Homrich <12125514+ehomrich@users.noreply.github.com>
2020-09-22 22:26:44 -0300

    Add GitHub API calls

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,33 +1,64 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
 
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const { data } = await api.get<Repository>(`repos/${newRepo}`);
+    console.log(data);
+    setRepositories([...repositories, data]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="GitHub Explorer" />
       <Title>Dashboard</Title>
 
-      <Form>
+      <Form onSubmit={handleAddRepository}>
         <input
           type="text"
           name="repository"
           placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
         />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img src={logoImg} alt="foo" />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>foo bar baz qux foo bar baz qux foo bar baz qux </p>
-          </div>
-          <FiChevronRight size="20" />
-        </a>
+        {repositories.map(
+          ({ full_name, description, owner: { avatar_url, login } }) => (
+            <a key={full_name} href="teste">
+              <img src={avatar_url} alt={login} />
+              <div>
+                <strong>{full_name}</strong>
+                <p>{description}</p>
+              </div>
+              <FiChevronRight size="20" />
+            </a>
+          ),
+        )}
       </Repositories>
     </>
   );
