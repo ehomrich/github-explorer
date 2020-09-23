a1cffdd30c847e0b99fb2c8637028f6fa372037d
Émerson P. Homrich <12125514+ehomrich@users.noreply.github.com>
2020-09-22 21:47:24 -0300

    Dashboard styling

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Dashboard</Title>
+
+      <Form>
+        <input
+          type="text"
+          name="repository"
+          placeholder="Digite o nome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src={logoImg} alt="foo" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>foo bar baz qux foo bar baz qux foo bar baz qux </p>
+          </div>
+          <FiChevronRight size="20" />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
